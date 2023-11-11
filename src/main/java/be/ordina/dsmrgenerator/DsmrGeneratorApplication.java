0d78948f7b705eae2f680f6fda82fcfadc0063d0
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-11-11 12:40:49 +0100

    first commit

---
+++ src/main/java/be/ordina/dsmrgenerator/DsmrGeneratorApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.dsmrgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmrGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmrGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ordina/dsmrgenerator/DsmrGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ordina.dsmrgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmrGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
