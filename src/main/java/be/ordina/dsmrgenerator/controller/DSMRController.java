0bbdd49b0a6534133a5f0187e821a3fb5da7f91a
GabrielDelaPena <89069807+GabrielDelaPena@users.noreply.github.com>
2023-11-14 11:35:12 +0100

    Update Package v7

--- src/main/java/be/ordina/dsmrgenerator/controller/DSMRController.java
+++ src/main/java/be/ordina/dsmrgenerator/controller/DSMRController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigInteger;
-import java.util.Base64;
 import java.util.List;
 
 @RequestMapping("/api/v4/generator")
--- src/main/java/be/ordina/dsmrgenerator/utils/FakeDataGenerator.java
+++ src/main/java/be/ordina/dsmrgenerator/utils/FakeDataGenerator.java
@@ -18,36 +18,36 @@ public class FakeDataGenerator {
             for (int interval = 0; interval < numIntervalsPerDay; interval++) {
                 DSMR digitalElectricityMeter = new DSMR();
 
-                digitalElectricityMeter.setSerialNumberElectricityMeter(String.valueOf(random.nextInt(10000)));
+                digitalElectricityMeter.setSerialNumberElectricityMeter("24");
                 digitalElectricityMeter.setTimestamp(currentTimestamp);
                 digitalElectricityMeter.setMessage("");
-                digitalElectricityMeter.setSwitchPositionElectricity(random.nextInt(2) + 1);
-                digitalElectricityMeter.setMaxAllowedPowerPerPhase(random.nextDouble() * 1000);
-                digitalElectricityMeter.setMaxAllowedCurrentPerPhase(random.nextDouble() * 100);
+                digitalElectricityMeter.setSwitchPositionElectricity(1);
+                digitalElectricityMeter.setMaxAllowedPowerPerPhase(999.9); // kW
+                digitalElectricityMeter.setMaxAllowedCurrentPerPhase(999); // A
 
                 // Set total consumption and production for two rates (day and night)
-                digitalElectricityMeter.setRate1TotalConsumption(random.nextDouble() * 1000);
-                digitalElectricityMeter.setRate2TotalConsumption(random.nextDouble() * 1000);
-                digitalElectricityMeter.setRate1TotalProduction(random.nextDouble() * 100);
-                digitalElectricityMeter.setRate2TotalProduction(random.nextDouble() * 100);
+                digitalElectricityMeter.setRate1TotalConsumption(random.nextInt(900-600) + 600); // kW
+                digitalElectricityMeter.setRate2TotalConsumption(random.nextInt(600-400) + 400);
+                digitalElectricityMeter.setRate1TotalProduction(random.nextInt(900-600) + 600);
+                digitalElectricityMeter.setRate2TotalProduction(random.nextInt(600-400) + 400);
 
                 // Set current values for all phases and L1, L2, L3
-                digitalElectricityMeter.setAllPhasesConsumption(random.nextDouble() * 1000);
-                digitalElectricityMeter.setAllPhasesProduction(random.nextDouble() * 100);
-                digitalElectricityMeter.setL1Consumption(random.nextDouble() * 100);
-                digitalElectricityMeter.setL2Consumption(random.nextDouble() * 100);
-                digitalElectricityMeter.setL3Consumption(random.nextDouble() * 100);
-                digitalElectricityMeter.setL1Production(random.nextDouble() * 100);
-                digitalElectricityMeter.setL2Production(random.nextDouble() * 100);
-                digitalElectricityMeter.setL3Production(random.nextDouble() * 100);
+                digitalElectricityMeter.setAllPhasesConsumption(Math.round(0.500 + (0.800 - 0.500) * random.nextDouble() * 1000.0) / 1000.0); // 00.200kW - 00.800kW
+                digitalElectricityMeter.setAllPhasesProduction(Math.round(0.500 + (0.800 - 0.500) * random.nextDouble() * 1000.0) / 1000.0); // 00.200kW - 00.800kW
+                digitalElectricityMeter.setL1Consumption(Math.round(0.500 + (0.800 - 0.500) * random.nextDouble() * 1000.0) / 1000.0); // 00.200kW - 00.800kW
+                digitalElectricityMeter.setL2Consumption(random.nextInt(5000 - 2000 + 1) + 2000); // 2,000kW - 5,000kW
+                digitalElectricityMeter.setL3Consumption(random.nextInt(5000 - 2000 + 1) + 2000); // 2,000kW - 5,000kW
+                digitalElectricityMeter.setL1Production(random.nextInt(5000 - 2000 + 1) + 2000); // 2,000kW - 5,000kW
+                digitalElectricityMeter.setL2Production(random.nextInt(5000 - 2000 + 1) + 2000); // 2,000kW - 5,000kW
+                digitalElectricityMeter.setL3Production(random.nextInt(5000 - 2000 + 1) + 2000); // 2,000kW - 5,000kW
 
                 // Set voltage and current values for L1, L2, L3
-                digitalElectricityMeter.setL1Voltage(random.nextDouble() * 240);
-                digitalElectricityMeter.setL2Voltage(random.nextDouble() * 240);
-                digitalElectricityMeter.setL3Voltage(random.nextDouble() * 240);
-                digitalElectricityMeter.setL1Current(random.nextDouble() * 20);
-                digitalElectricityMeter.setL2Current(random.nextDouble() * 20);
-                digitalElectricityMeter.setL3Current(random.nextDouble() * 20);
+                digitalElectricityMeter.setL1Voltage(random.nextInt(230-220) + 220); // V
+                digitalElectricityMeter.setL2Voltage(random.nextInt(230-220) + 220);
+                digitalElectricityMeter.setL3Voltage(random.nextInt(230-220) + 220);
+                digitalElectricityMeter.setL1Current(random.nextInt(10-1) + 1); // A
+                digitalElectricityMeter.setL2Current(random.nextInt(10-1) + 1);
+                digitalElectricityMeter.setL3Current(random.nextInt(10-1) + 1);
 
                 digitalElectricityMeter.setOtherDevicesOnBus(random.nextInt(10));
                 fakeData.add(digitalElectricityMeter);
